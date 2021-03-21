8e5117776d8ed21a8db138d7b883de9e8c4d9899
IZVUI <valerazolotov2000@gmail.com>
2021-03-21 21:08:17 +0300

    WorkArea and Button components added

--- src/calculator/Main.js
+++ src/calculator/Main.js
@@ -1,11 +1,22 @@
 import React, {Component} from "react";
+import WorkArea from "./WorkArea";
 
 class Main extends Component {
 
-    render() {
-        return(
-            <div>
+    state = {}
 
+    render() {
+        return (
+            <div style={
+                {
+                    width: 500,
+                    height: 600,
+                    border: '1px solid black',
+                    marginLeft: 600,
+                    marginTop: 100
+                }
+            }>
+                <WorkArea />
             </div>
         )
     }
---
+++ src/calculator/WorkArea.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div>
+            <input style={{height: 200, width: 497, fontSize: 50, textAlign: "right"}}/>
+        </div>
+    )
+}
