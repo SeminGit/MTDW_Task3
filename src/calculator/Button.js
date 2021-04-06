ae5d50453be0c02528dd7cab39f93507a0acecf0
Vladimir Karpov <uladzimir_karpau@senla.eu>
2021-04-06 22:55:12 +0300

    Стили v2

--- src/calculator/Button.js
+++ src/calculator/Button.js
@@ -1,19 +1,8 @@
 import React from "react";
+import Styles from "./Styles.css";
 
 export default (props) => {
     return (
-      <button onClick={props.clickHandler} style={
-          props.style ? props.style :
-            {
-                width: "-webkit-fill-available",
-                height: 50,
-                margin: 5,
-                background: 'linear-gradient(166.66deg, #328CD0 25.32%, #081925 250.71%)',
-                borderColor: '#081925',
-                color: "white",
-                fontSize: 20,
-                borderRadius: 6
-            }
-      } value={props.text}>{props.text}</button>
+      <button onClick={props.clickHandler} className={Styles.button} value={props.text}>{props.text}</button>
     )
 }
