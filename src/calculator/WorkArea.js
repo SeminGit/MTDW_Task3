5f36be2fa0d847b3754f904f8d2504cae26e37da
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2021-04-06 20:59:57 +0300

    initial

--- src/calculator/WorkArea.js
+++ src/calculator/WorkArea.js
@@ -2,70 +2,52 @@ import React, {Component} from "react";
 
 export default class WorkArea extends Component {
 
-    state = {
-        text:this.props.content
+  state = {
+    text: this.props.content
+  }
+
+  toFocus = () => {
+    let event = document.getElementById('workArea')
+    // event.focus();
+    // event.selectionStart = event.value.length + 1;
+    event.value = this.props.content;
+    if (event.setSelectionRange) {
+      event.setSelectionRange(this.props.content.length, this.props.content.length)
     }
+    event.focus();
+    // this.setState(
+    //     {
+    //         text:this.props.content
+    //     }
+    // )
+    return this.props.content;
+  }
 
-    toFocus = () => {
-        let event = document.getElementById('workArea')
-       // event.focus();
-       // event.selectionStart = event.value.length + 1;
-        event.value = this.props.content;
-        if (event.setSelectionRange) {
-            event.setSelectionRange(this.props.content.length, this.props.content.length)
-        }
-        event.focus();
-        // this.setState(
-        //     {
-        //         text:this.props.content
-        //     }
-        // )
-        return this.props.content;
+  focusing = () => {
+    let event = document.getElementById('workArea')
+    if (event.setSelectionRange) {
+      event.setSelectionRange(event.value.length, event.value.length)
     }
+    event.focus();
+  }
 
-    focusing = () => {
-        let event = document.getElementById('workArea')
-        if (event.setSelectionRange) {
-            event.setSelectionRange(event.value.length, event.value.length)
-        }
-        event.focus();
-    }
+  render = () => {
+    return (
 
-    render = () => {
-        return (
-
-            <div>
-                <input id='workArea' style={{
-                    height: 200
-                    ,
-                    width: 350
-                    ,
-                    fontSize: 70
-                    ,
-                    textAlign: "right"
-                    ,
-                    paddingTop: 110
-                    ,
-                    backgroundColor: 'rgba(1,1,1,0)'
-                    ,
-                    border: 0
-                    ,
-                    boxSizing: "border-box"
-                    ,
-                    color: 'white'
-                    ,
-                    outline:'none'
-                }
-                }
-                       value=
-                           {
-                               this.props.content == this.state.text ? this.props.content :
-                                   this.toFocus()
-                           }
-                       onfocusin={this.focusing}
-
-                />
-            </div>
-        )
-    }
+      <div>
+        <input
+          id='workArea'
+          style={{
+            height: 200, width: 350, fontSize: 70,
+            textAlign: "right", paddingTop: 110,
+            backgroundColor: 'rgba(1,1,1,0)', border: 0,
+            boxSizing: "border-box", color: 'white', outline: 'none'
+          }
+          } value={this.props.content == this.state.text ? this.props.content : this.toFocus()}
+          onfocusin={this.focusing}
+          disabled
+        />
+      </div>
+    )
+  }
 }
